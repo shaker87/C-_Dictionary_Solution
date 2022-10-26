d833312c6a12065065fa7fb11bf5ab1dea7b6e69
Shaker Hossain <73550448+shaker87@users.noreply.github.com>
2022-10-26 19:16:54 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace dicApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            hashtable.Add("pen", "কলম");
+            hashtable.Add("book", "বই");
+
+            string word = wordTextBox.Text.ToLower();
+
+             foreach(string key in hashtable.Keys){
+                 if(word.Equals(key)){
+                    meanTextBox.Text = hashtable[key].ToString();
+                     break;
+                    }
+                 else{
+                    meanTextBox.Text = "Unknown";
+                 }
+             };
+        }
+
+       
+    }
+}
